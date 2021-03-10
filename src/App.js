5b496068cff9d04cadd890435bed3d743cfe4b98
adkuca <34562899+adkuca@users.noreply.github.com>
2021-03-10 16:03:36 +0100

    Add styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import { useEffect, useState, useMemo } from 'react';
-import './App.css';
+import './app.scss';
 import {getWeather, getForecast} from './adapters/openweathermap.adapter.js';
 import Layout from './components/Layout/Layout.jsx';
 import City from './components/City/City.jsx';
 import degToCardinalDir from './helpers/degToCardinalDirection.js';
-import React from 'react';
 
 const getWeatherEarly = getWeather();
 
@@ -18,33 +17,36 @@ function App() {
     const capitalizedDescrip = weatherData.weather && (s => s.substr(0, 1).toUpperCase() + s.substr(1))(weatherData.weather[0].description);
 
     const rightWeatherInfoBlock = (
-        <>
-          <span style={{display: 'block'}} key={1}>Feels Like: {weatherData.main && Math.round(weatherData.main.feels_like)}° C</span>
-          <span style={{display: 'block'}} key={2}>Pressure: {weatherData.main && weatherData.main.pressure} hPa</span>
-          <span style={{display: 'block'}} key={3}>Humidity: {weatherData.main && weatherData.main.humidity}%</span>
-          <span style={{display: 'block'}} key={4}>Feels Like: {weatherData.wind && Math.round(weatherData.wind.speed)} km/h, {cardinalWindDir}</span>
-        </>
+        <ul className="main-info__list">
+          <li key={1} className="main-info__item">Feels Like: {weatherData.main && Math.round(weatherData.main.feels_like)}° C</li>
+          <li key={2} className="main-info__item">Pressure: {weatherData.main && weatherData.main.pressure} hPa</li>
+          <li key={3} className="main-info__item">Humidity: {weatherData.main && weatherData.main.humidity}%</li>
+          <li key={4} className="main-info__item">Feels Like: {weatherData.wind && Math.round(weatherData.wind.speed)} km/h, {cardinalWindDir}</li>
+        </ul>
     )
 
     return {capitalizedDescrip, rightWeatherInfoBlock};
   }, [weatherData]);
 
   const forecastDataDisplay = useMemo(() => {
-    const forecastBoxItem = (day) => (
-      <li key={day.dt}>
-        <span>{new Date(day.dt * 1000).toLocaleDateString(undefined, {weekday: 'short'})}</span>
-        <span>{Math.round(day.main.temp)}°</span>
-        <span>{day.weather[0].description}</span>
-      </li>
-    )
+    const forecastBoxItem = (day) => {
+      const capitalizedWeatherDescrip = (s => s.substr(0, 1).toUpperCase() + s.substr(1))(day.weather[0].description);
+      return (
+        <li key={day.dt} className="forecast-box__item">
+          <span className="forecast-box__weekday">{new Date(day.dt * 1000).toLocaleDateString(undefined, {weekday: 'short'})}</span>
+          <span className="forecast-box__temp">{Math.round(day.main.temp)}°</span>
+          <span className="forecast-box__description">{capitalizedWeatherDescrip}</span>
+        </li>
+      )
+    }
 
     const forecastBox = (
-      <>
-        <h2>5 Day forecast</h2>
-        <ul>
+      <div className="forecast-box">
+        <h2 className="forecast-box__title">5 Day forecast</h2>
+        <ul className="forecast-box__list">
           {forecastData.map(forecastBoxItem)}
         </ul>
-      </>
+      </div>
     )
 
     return {forecastBox}
@@ -81,18 +83,22 @@ function App() {
     <div className="App">
       <Layout>
         <City>
-            <div>
-              <h2>{weatherData.name}, {weatherDataDisplay.capitalizedDescrip}</h2>
-              <img src={weatherData.weather && `http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} alt=""/>
-            </div>
-            <div>
-              <span>{weatherData.main && Math.round(weatherData.main.temp)}°</span>
-              <div>
+          <div className="container">
+            <div className="">
+              <div className="heading">
+                <h2 className="heading__city">{weatherData.name}, {weatherDataDisplay.capitalizedDescrip}</h2>
+                <div className="heading__img-wrapper">
+                  <img className="heading__img" src={weatherData.weather && `http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} alt=""/>
+                </div>
+              </div>
+              <div className="main-info">
+                <h3 className="main-info__temp">{weatherData.main && Math.round(weatherData.main.temp)}°</h3>
                 {weatherDataDisplay.rightWeatherInfoBlock}
               </div>
             </div>
-            <button onClick={openForecastHandler}>{isForecastOpen ? '-' : '+'}</button>
+            <button className="open-forecast-btn" onClick={openForecastHandler}>{isForecastOpen ? '-' : '+'}</button>
             {isForecastOpen && forecastDataDisplay.forecastBox}
+          </div>
         </City>
       </Layout>
     </div>
